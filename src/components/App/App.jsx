5c109b6ff783e5d1b9f435c723d5a243a21fa44b
Lyubomyr Klymyuk <lyubomyrklymyuk2001@gmail.com>
2021-11-18 22:01:29 +0200

    add increase and rise toogle funtion

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,22 +1,66 @@
-import './App.css';
+import {useState} from 'react';
+
 import AppInfo from "../app-info/app-info";
 import SearchPanel from "../search-panel/search-panel";
 import AppFilter from "../app-filter/app-filter";
 import EmployeesList from "../employees-list/employees-list";
 import EmployeesAddForm from "../employees-add-form/employees-add-form";
-function App() {
 
-  return (
-   <div className="app">
-       <AppInfo/>
-       <div className="search-panel">
-           <SearchPanel/>
-           <AppFilter/>
+import './App.css';
+
+
+function App() {
+    let maxId = 4
+
+    const [data, setData] = useState([
+        {name: "Anna Kostiuk", salary: 800, increase: false, rise: false, id: 1},
+        {name: "Inna Kostiuk", salary: 1232, increase: false, rise: false, id: 2},
+        {name: "Alla Kostiuk", salary: 4400, increase: true, rise: true, id: 3}
+    ])
+
+
+    const deleteItem = (id) => {
+        setData(data.filter(item => item.id !== id))
+    }
+
+    const addItem = (name,salary ) => {
+
+        let item = {
+            name,
+            salary,
+            increase: false,
+            rise: false,
+            id: maxId
+        }
+
+        setData([...data, item])
+    }
+
+    const onToggleProp = (id, prop) => {
+        setData(data.map(item=>{
+            if(item.id === id){
+                return{...item, [prop]: !item[prop]}
+            }
+            return item
+        }))
+    }
+
+
+    return (
+       <div className="app">
+           <AppInfo/>
+           <div className="search-panel">
+               <SearchPanel/>
+               <AppFilter/>
+           </div>
+           <EmployeesList data={data}
+                          onDelete={deleteItem}
+                          onToggleProp={onToggleProp}
+
+                          />
+           <EmployeesAddForm onAdd={addItem}/>
        </div>
-       <EmployeesList/>
-       <EmployeesAddForm/>
-   </div>
-  );
+    );
 }
 
 export default App;
--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,6 +1,30 @@
 import './employees-add-form.css';
+import {useState} from "react";
 
-const EmployeesAddForm = () => {
+const EmployeesAddForm = ({onAdd}) => {
+    const [name, setName] = useState('')
+    const [salary, setSalary] = useState('')
+
+    const onValueChange = (e) =>{
+        switch (e.target.name){
+            case 'name':
+                setName(e.target.value)
+                break;
+            case 'salary':
+                setSalary(e.target.value)
+                break;
+            default:
+                console.log(`Sorry, we are out of ${e.target}.`);
+        }
+    }
+
+    const onAddItem = (e) =>{
+        e.preventDefault();
+        onAdd(name, salary);
+        setName('');
+        setSalary('')
+    }
+
     return (
         <div className="app-add-form">
             <h3>Добавьте нового сотрудника</h3>
@@ -8,13 +32,20 @@ const EmployeesAddForm = () => {
                 className="add-form d-flex">
                 <input type="text"
                     className="form-control new-post-label"
-                    placeholder="Как его зовут?" />
+                    placeholder="Как его зовут?"
+                    name="name"
+                    value={name}
+                    onChange={onValueChange}/>
                 <input type="number"
                     className="form-control new-post-label"
-                    placeholder="З/П в $?" />
+                    placeholder="З/П в $?"
+                    name="salary"
+                    value={salary}
+                    onChange={onValueChange}/>
 
                 <button type="submit"
-                        className="btn btn-outline-light">Добавить</button>
+                        className="btn btn-outline-light"
+                        onClick={onAddItem}>Добавить</button>
             </form>
         </div>
     )
